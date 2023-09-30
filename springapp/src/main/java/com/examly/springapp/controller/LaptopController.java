ee799a2e5110b9675f451e1ce38d20e290bee110
Iamneo <techadmin@iamneo.ai>
2023-09-30 07:19:56 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/LaptopController.java
+++ springapp/src/main/java/com/examly/springapp/controller/LaptopController.java
@@ -1,3 +1,31 @@
+package com.examly.springapp.controller;
+
+import com.examly.springapp.model.Laptop;
+import com.examly.springapp.service.ApiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/laptops")
 public class LaptopController {
-    
+
+    @Autowired
+    private ApiService apiService;
+
+    @PostMapping("/")
+    public boolean addLaptop(@RequestBody Laptop laptop) {
+        return apiService.addLaptop(laptop);
+    }
+
+    @GetMapping("/{laptopid}")
+    public Laptop getLaptop(@PathVariable int laptopid) {
+        return apiService.getLaptopById(laptopid);
+    }
+
+    @GetMapping("/")
+    public List<Laptop> getAllLaptops() {
+        return apiService.getAllLaptops();
+    }
 }
--- springapp/src/main/java/com/examly/springapp/service/ApiService.java
+++ springapp/src/main/java/com/examly/springapp/service/ApiService.java
@@ -1,3 +1,32 @@
+package com.examly.springapp.service;
+
+import com.examly.springapp.model.Laptop;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class ApiService {
-    
+
+    private List<Laptop> laptops = new ArrayList<>();
+
+    public boolean addLaptop(Laptop laptop) {
+        // Implement logic to add a laptop to the list
+        return laptops.add(laptop);
+    }
+
+    public Laptop getLaptopById(int laptopid) {
+        // Implement logic to retrieve a laptop by ID
+        for (Laptop laptop : laptops) {
+            if (laptop.getLaptopid() == laptopid) {
+                return laptop;
+            }
+        }
+        return null; // Return null if not found
+    }
+
+    public List<Laptop> getAllLaptops() {
+        return laptops;
+    }
 }
